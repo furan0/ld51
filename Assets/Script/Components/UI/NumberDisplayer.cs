21f4172deba52ac7ed962424dfd426816a501ad7
Guillemain <34304992+Guillemain@users.noreply.github.com>
2022-10-02 15:10:48 +0200

    PV animation. + Hurted effect

--- Assets/Script/Components/UI/NumberDisplayer.cs
+++ Assets/Script/Components/UI/NumberDisplayer.cs
@@ -6,12 +6,18 @@ public class NumberDisplayer : MonoBehaviour
 {
 
     int counter = 0;
+    [SerializeField] bool desactivate = true;
     [SerializeField] GameObject[] item_list;
     // Start is called before the first frame update
     void Awake()
     {
         for (int i = 0 ;  (i<item_list.Length); i++ ){
-            item_list[i].SetActive(false);
+            if(desactivate){
+                item_list[i].SetActive(false);
+            }else{
+                item_list[i].GetComponent<Animator>()?.SetBool("Mort",true); 
+            }
+            //.SetActive(false);
         }
     }
 
@@ -24,14 +30,25 @@ public class NumberDisplayer : MonoBehaviour
     }
 
     public void updateDisplay(){
-        foreach (var item in item_list)
-        {
-            item.SetActive(false);
+        // foreach (var item in item_list)
+        // {
+        //     item.SetActive(false);
+        // }
+        for (int i = 0; (i < counter) && (i<item_list.Length); i++ ){
+            if(desactivate){
+                item_list[i].SetActive(true);
+            }else{
+                item_list[i].GetComponent<Animator>()?.SetBool("Mort",false); 
+            }
+        }
+        for (int j = counter ; j<item_list.Length ; j++ ){
+            if(desactivate){
+                item_list[j].SetActive(false);
+            }else{
+                item_list[j].GetComponent<Animator>()?.SetBool("Mort",true); 
+            }
         }
 
-        for (int i = 0 ; (i <counter) && (i<item_list.Length); i++ ){
-            item_list[i].SetActive(true);
-        }
     }
 
 }
